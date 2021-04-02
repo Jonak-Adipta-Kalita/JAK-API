22b87f7e0c106e536b30ae0662c87fa5e6fae572
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-04-02 12:33:51 +0530

    added api folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('api/db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 

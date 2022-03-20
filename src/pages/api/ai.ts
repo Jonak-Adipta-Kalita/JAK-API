8a363b8f3bb7da1c788ae01c6eabe6802c6075f9
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-03-20 19:09:45 +0530

    time to connect the model to the /api/ai endpoint

--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import * as tensorflow from "@tensorflow/tfjs";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     if (!(req.method === "POST")) {
@@ -7,5 +9,11 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
             .status(405)
             .json({ error: `Method ${req.method} not allowed` });
     }
+
+    const { message } = req.body;
+
+    const words = fs.readFileSync("../../ai/model/words.pkl", "binary");
+    const classes = fs.readFileSync("../../ai/model/classes.pkl", "binary");
+
     res.status(200).json({});
 };

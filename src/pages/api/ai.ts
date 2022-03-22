06fb39367707bbcf7cfa231693a122c4904c0ec2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-03-22 10:14:28 +0530

    its harder then i thought.....

--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
 import * as tensorflow from "@tensorflow/tfjs";
 import intent from "../../ai/model/intents.json";
 
@@ -11,9 +12,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const { message } = req.body;
+    const words = fs.readFileSync("../../ai/model/words.pkl", "binary");
+    const classes = fs.readFileSync("../../ai/model/classes.pkl", "binary");
     const modal = tensorflow.loadLayersModel(
         "https://raw.githubusercontent.com/Jonak-Adipta-Kalita/JAK-API/main/src/ai/model.json"
     );
+
+    const clean_up_sentence = (sentence: string) => {};
+    const bag_of_words = (sentence: string) => {};
+    const predict_class = (sentence: string) => {};
+    const get_response = (intents_list: [], intents_json: object) => {};
 
     res.status(200).json({});
 };

ee55d5658b3ad8e332019d32d392185fdc713a3d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-25 14:31:15 +0530

    reverted?

---
+++ src/data/ben10/omnitrix.ts
@@ -0,0 +1,486 @@
+import { Ben10 } from "../../typings";
+
+const omnitrix: Ben10["omnitrix"] = [
+    {
+        id: 0,
+        name: "Cannonbolt",
+        image: "/images/ben10/cannonbolt.png",
+    },
+    {
+        id: 1,
+        name: "Overflow",
+        image: "/images/ben10/overflow.png",
+    },
+    {
+        id: 2,
+        name: "Heatblast",
+        image: "/images/ben10/heatblast.png",
+    },
+    {
+        id: 3,
+        name: "XLR8",
+        image: "/images/ben10/xlr8.png",
+    },
+    {
+        id: 4,
+        name: "Four Arms",
+        image: "/images/ben10/four-arms.png",
+    },
+    {
+        id: 5,
+        name: "Grey Matter",
+        image: "/images/ben10/grey-matter.png",
+    },
+    {
+        id: 6,
+        name: "Diamondhead",
+        image: "/images/ben10/diamondhead.png",
+    },
+    {
+        id: 7,
+        name: "Upgrade",
+        image: "/images/ben10/upgrade.png",
+    },
+    {
+        id: 8,
+        name: "Stinkfly",
+        image: "/images/ben10/stinkfly.png",
+    },
+    {
+        id: 9,
+        name: "Wildvine",
+        image: "/images/ben10/wildvine.png",
+    },
+    {
+        id: 10,
+        name: "Shock Rock",
+        image: "/images/ben10/shock-rock.png",
+    },
+    {
+        id: 11,
+        name: "Slapback",
+        image: "/images/ben10/slapback.png",
+    },
+    {
+        id: 12,
+        name: "Humungousaur",
+        image: "/images/ben10/humungousaur.png",
+    },
+    {
+        id: 13,
+        name: "Rath",
+        image: "/images/ben10/rath.png",
+    },
+    {
+        id: 14,
+        name: "Jetray",
+        image: "/images/ben10/jetray.png",
+    },
+    {
+        id: 15,
+        name: "Goop",
+        image: "/images/ben10/goop.png",
+    },
+    {
+        id: 16,
+        name: "Way Big",
+        image: "/images/ben10/way-big.png",
+    },
+    {
+        id: 17,
+        name: "Surge",
+        image: "/images/ben10/surge.png",
+    },
+    {
+        id: 18,
+        name: "Alien X",
+        image: "/images/ben10/alien-x.png",
+    },
+    {
+        id: 19,
+        name: "Ghostfreak",
+        image: "/images/ben10/ghostfreak.png",
+    },
+    {
+        id: 20,
+        name: "Archfiendzarro",
+        image: "/images/ben10/archfiendzarro.png",
+    },
+    {
+        id: 21,
+        name: "Arctiguana",
+        image: "/images/ben10/arctiguana.png",
+    },
+    {
+        id: 22,
+        name: "Astrodactyl",
+        image: "/images/ben10/astrodactyl.png",
+    },
+    {
+        id: 23,
+        name: "Armodrillo",
+        image: "/images/ben10/armodrillo.png",
+    },
+    {
+        id: 24,
+        name: "Atomix",
+        image: "/images/ben10/atomix.png",
+    },
+    {
+        id: 25,
+        name: "Atomix-X",
+        image: "/images/ben10/atomix-x.png",
+    },
+    {
+        id: 26,
+        name: "Ball Weevil",
+        image: "/images/ben10/ball-weevil.png",
+    },
+    {
+        id: 27,
+        name: "Bat Dude",
+        image: "/images/ben10/bat-dude.png",
+    },
+    {
+        id: 28,
+        name: "Big Chill",
+        image: "/images/ben10/big-chill.png",
+    },
+    {
+        id: 29,
+        name: "Big Chuck",
+        image: "/images/ben10/big-chuck.png",
+    },
+    {
+        id: 30,
+        name: "Blitzwolfer",
+        image: "/images/ben10/blitzwolfer.png",
+    },
+    {
+        id: 31,
+        name: "Bloxx",
+        image: "/images/ben10/bloxx.png",
+    },
+    {
+        id: 32,
+        name: "Bob The Blob",
+        image: "/images/ben10/bob-the-blob.png",
+    },
+    {
+        id: 33,
+        name: "BrainDrain",
+        image: "/images/ben10/braindrain.png",
+    },
+    {
+        id: 34,
+        name: "BrainStorm",
+        image: "/images/ben10/brainstorm.png",
+    },
+    {
+        id: 35,
+        name: "BullFrag",
+        image: "/images/ben10/bullfrag.png",
+    },
+    {
+        id: 36,
+        name: "Bungee Sponge",
+        image: "/images/ben10/bungee-sponge.png",
+    },
+    {
+        id: 37,
+        name: "BuzzShock",
+        image: "/images/ben10/buzzshock.png",
+    },
+    {
+        id: 38,
+        name: "Water Hazard",
+        image: "/images/ben10/water-hazard.png",
+    },
+    {
+        id: 39,
+        name: "Chamalien",
+        image: "/images/ben10/chamalien.png",
+    },
+    {
+        id: 40,
+        name: "Chromastone",
+        image: "/images/ben10/chromastone.png",
+    },
+    {
+        id: 41,
+        name: "ClockWork",
+        image: "/images/ben10/clockwork.png",
+    },
+    {
+        id: 42,
+        name: "CrashHopper",
+        image: "/images/ben10/crashhopper.png",
+    },
+    {
+        id: 43,
+        name: "Crashocker",
+        image: "/images/ben10/crashocker.png",
+    },
+    {
+        id: 44,
+        name: "Deefus Veeblepister",
+        image: "/images/ben10/deefus-veeblepister.png",
+    },
+    {
+        id: 45,
+        name: "Diamond Matter",
+        image: "/images/ben10/diamond-matter.png",
+    },
+    {
+        id: 46,
+        name: "Ditto",
+        image: "/images/ben10/ditto.png",
+    },
+    {
+        id: 47,
+        name: "Dog Nabbit",
+        image: "/images/ben10/dog-nabbit.png",
+    },
+    {
+        id: 48,
+        name: "Eatle",
+        image: "/images/ben10/eatle.png",
+    },
+    {
+        id: 49,
+        name: "Echo Echo",
+        image: "/images/ben10/echo-echo.png",
+    },
+    {
+        id: 50,
+        name: "Eye Guy",
+        image: "/images/ben10/eye-guy.png",
+    },
+    {
+        id: 51,
+        name: "ElectricYeti",
+        image: "/images/ben10/electricyeti.png",
+    },
+    {
+        id: 52,
+        name: "FastTrack",
+        image: "/images/ben10/fasttrack.png",
+    },
+    {
+        id: 53,
+        name: "FeedBack",
+        image: "/images/ben10/feedback.png",
+    },
+    {
+        id: 54,
+        name: "Fish Fingers",
+        image: "/images/ben10/fish-fingers.png",
+    },
+    {
+        id: 55,
+        name: "FrankenStrike",
+        image: "/images/ben10/frankenstrike.png",
+    },
+    {
+        id: 56,
+        name: "FreezeGhost",
+        image: "/images/ben10/freezeghost.png",
+    },
+    {
+        id: 57,
+        name: "FreezeLizard",
+        image: "/images/ben10/freezelizard.png",
+    },
+    {
+        id: 58,
+        name: "Galvan Major",
+        image: "/images/ben10/galvan-major.png",
+    },
+    {
+        id: 59,
+        name: "Giant Manster",
+        image: "/images/ben10/giant-manster.png",
+    },
+    {
+        id: 60,
+        name: "Gravattack",
+        image: "/images/ben10/gravattack.png",
+    },
+    {
+        id: 61,
+        name: "Gutrot",
+        image: "/images/ben10/gutrot.png",
+    },
+    {
+        id: 62,
+        name: "Grey Arms",
+        image: "/images/ben10/grey-arms.png",
+    },
+    {
+        id: 63,
+        name: "Handy Man",
+        image: "/images/ben10/handy-man.png",
+    },
+    {
+        id: 64,
+        name: "Humungoopsaur",
+        image: "/images/ben10/humungoopsaur.png",
+    },
+    {
+        id: 65,
+        name: "Juryrigg",
+        image: "/images/ben10/juryrigg.png",
+    },
+    {
+        id: 66,
+        name: "Kickin Hawk",
+        image: "/images/ben10/kickin-hawk.png",
+    },
+    {
+        id: 67,
+        name: "Lightning Volt",
+        image: "/images/ben10/lightning-volt.png",
+    },
+    {
+        id: 68,
+        name: "Lodestar",
+        image: "/images/ben10/lodestar.png",
+    },
+    {
+        id: 69,
+        name: "Mole Stache",
+        image: "/images/ben10/mole-stache.png",
+    },
+    {
+        id: 70,
+        name: "Mr. Monkey",
+        image: "/images/ben10/mr-monkey.png",
+    },
+    {
+        id: 71,
+        name: "Mr. Mucky",
+        image: "/images/ben10/mr-mucky.png",
+    },
+    {
+        id: 72,
+        name: "Nanomech",
+        image: "/images/ben10/nanomech.png",
+    },
+    {
+        id: 73,
+        name: "Nighty Knight",
+        image: "/images/ben10/nighty-knight.png",
+    },
+    {
+        id: 74,
+        name: "NRG",
+        image: "/images/ben10/nrg.png",
+    },
+    {
+        id: 75,
+        name: "Pesky Dust",
+        image: "/images/ben10/pesky-dust.png",
+    },
+    {
+        id: 76,
+        name: "Ripjaws",
+        image: "/images/ben10/ripjaws.png",
+    },
+    {
+        id: 77,
+        name: "Rollaway",
+        image: "/images/ben10/rollaway.png",
+    },
+    {
+        id: 78,
+        name: "Shocksquatch",
+        image: "/images/ben10/shocksquatch.png",
+    },
+    {
+        id: 79,
+        name: "Snare-oh",
+        image: "/images/ben10/snare-oh.png",
+    },
+    {
+        id: 80,
+        name: "Spidermonkey",
+        image: "/images/ben10/spidermonkey.png",
+    },
+    {
+        id: 81,
+        name: "SpeedyQuick",
+        image: "/images/ben10/speedyquick.png",
+    },
+    {
+        id: 82,
+        name: "Spitter",
+        image: "/images/ben10/Spitter.png",
+    },
+    {
+        id: 83,
+        name: "Stink Arms",
+        image: "/images/ben10/stink-arms.png",
+    },
+    {
+        id: 84,
+        name: "SwampFire",
+        image: "/images/ben10/swampfire.png",
+    },
+    {
+        id: 85,
+        name: "Techno-Bubble",
+        image: "/images/ben10/techno-bubble.png",
+    },
+    {
+        id: 86,
+        name: "Terraspin",
+        image: "/images/ben10/terraspin.png",
+    },
+    {
+        id: 87,
+        name: "The Worst",
+        image: "/images/ben10/the-worst.png",
+    },
+    {
+        id: 88,
+        name: "Toepick",
+        image: "/images/ben10/toepick.png",
+    },
+    {
+        id: 89,
+        name: "Toolboxx",
+        image: "/images/ben10/toolboxx.png",
+    },
+    {
+        id: 90,
+        name: "Upchuck",
+        image: "/images/ben10/upchuck.png",
+    },
+    {
+        id: 91,
+        name: "Uprigg",
+        image: "/images/ben10/uprigg.png",
+    },
+    {
+        id: 92,
+        name: "Vomit Man",
+        image: "/images/ben10/vomit-man.png",
+    },
+    {
+        id: 93,
+        name: "Walkatrout",
+        image: "/images/ben10/walkatrout.png",
+    },
+    {
+        id: 94,
+        name: "Whampire",
+        image: "/images/ben10/whampire.png",
+    },
+    {
+        id: 95,
+        name: "Wildmutt",
+        image: "/images/ben10/wildmutt.png",
+    },
+];
+
+export default omnitrix;
---
+++ src/data/genshinImpact/elements.ts
@@ -0,0 +1,55 @@
+import { GenshinImpact } from "../../typings";
+
+const elements: GenshinImpact["elements"] = [
+    {
+        id: 0,
+        name: "Pyro",
+        image: "/images/genshinImpact/elements/pyro.png",
+        archon: "Murata",
+        status_applies: "Pyro",
+    },
+    {
+        id: 1,
+        name: "Hydro",
+        image: "/images/genshinImpact/elements/hydro.png",
+        archon: "Hydro Archon",
+        status_applies: "Wet",
+    },
+    {
+        id: 2,
+        name: "Anemo",
+        image: "/images/genshinImpact/elements/anemo.png",
+        archon: "Barbatos",
+        status_applies: null,
+    },
+    {
+        id: 3,
+        name: "Electro",
+        image: "/images/genshinImpact/elements/electro.png",
+        archon: "Baal",
+        status_applies: "Electro",
+    },
+    {
+        id: 4,
+        name: "Dendro",
+        image: "/images/genshinImpact/elements/dendro.png",
+        archon: "Dendro Archon",
+        status_applies: "Dendro",
+    },
+    {
+        id: 5,
+        name: "Cyro",
+        image: "/images/genshinImpact/elements/cyro.png",
+        archon: "Tsaritsa",
+        status_applies: "Cryo",
+    },
+    {
+        id: 6,
+        name: "Geo",
+        image: "/images/genshinImpact/elements/geo.png",
+        archon: "Morax",
+        status_applies: null,
+    },
+];
+
+export default elements;

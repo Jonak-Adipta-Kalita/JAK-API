0f85ba0248355b874624713ec7b34fb2c8200f9b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-15 19:21:28 +0530

    gonna use my website's backend for it

--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,6 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { spawn } from "child_process";
-import * as tensorflow from "@tensorflow/tfjs";
 
 /**
  * @swagger
@@ -29,25 +27,6 @@ export default async (
     }
 
     const { message }: { message: string } = req.body;
-
-    // const model = await tensorflow.loadLayersModel(
-    //     "http://localhost:8000/model/model.json"
-    // );
-
-    // const prediction = model.predict(
-    //     tensorflow.tensor2d([message], [1, message.length])
-    // ) as tensorflow.Tensor;
-
-    // const index = prediction.argMax(1).dataSync()[0];
-
-    // const response = intent[index].responses;
-
-    const pythonProcess = spawn("python", [
-        "F:\\Hard-disk\\Project\\Works\\JAK API\\src\\ai\\response.py",
-        message,
-    ]);
-    pythonProcess.stdout.on("data", (data: any) => {
-        const reply = data.toString();
-        res.status(200).json(reply);
-    });
+	
+	res.status(200).json(reply);
 };

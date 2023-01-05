2a1aa39618d017a50e78cdc649904596b4bf51d7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-01-05 14:38:25 +0530

    renamed files

--- src/api.ts
+++ src/api.ts
@@ -9,21 +9,21 @@ import {
 } from "./typings";
 import dotenv from "dotenv";
 
-import omnitrix from "./data/ben10/omnitrix";
-import brawlers from "./data/brawlStars/brawlers";
-import players from "./data/brawlStars/players";
-import characters from "./data/genshinImpact/characters";
-import elements from "./data/genshinImpact/elements";
-import fav_food from "./data/jak/fav_food";
-import games from "./data/jak/games";
-import hobby from "./data/jak/hobby";
-import pictures from "./data/jak/pictures";
-import social_medias from "./data/jak/social_medias";
-import akumatized from "./data/miraculous/akumatized";
-import amokatized from "./data/miraculous/amokatized";
-import holders from "./data/miraculous/holders";
-import kwamis from "./data/miraculous/kwamis";
-import kings from "./data/mughalEmpire/kings";
+import ben10Omnitrix from "./data/ben10/omnitrix";
+import brawlStarsBrawlers from "./data/brawlStars/brawlers";
+import brawlStarsPlayers from "./data/brawlStars/players";
+import genshinImpactCharacters from "./data/genshinImpact/characters";
+import genshinImpactElements from "./data/genshinImpact/elements";
+import jakFavFood from "./data/jak/fav_food";
+import jakGames from "./data/jak/games";
+import jakHobby from "./data/jak/hobby";
+import jakPictures from "./data/jak/pictures";
+import jakSocialMedias from "./data/jak/social_medias";
+import miraculousAkumatized from "./data/miraculous/akumatized";
+import miraculousAmokatized from "./data/miraculous/amokatized";
+import miraculousHolders from "./data/miraculous/holders";
+import miraculousKwamis from "./data/miraculous/kwamis";
+import mughalEmpireKings from "./data/mughalEmpire/kings";
 
 dotenv.config();
 const app = express();
@@ -42,8 +42,8 @@ app.get(
                 .json({ error: `Method ${req.method} not allowed` });
         }
         res.status(200).json({
-            character: characters,
-            elements: elements,
+            character: genshinImpactCharacters,
+            elements: genshinImpactElements,
         });
     }
 );
@@ -56,7 +56,7 @@ app.get("/api/ben10", async (req, res: Response<Ben10 | { error: string }>) => {
             .json({ error: `Method ${req.method} not allowed` });
     }
     res.status(200).json({
-        omnitrix: omnitrix,
+        omnitrix: ben10Omnitrix,
     });
 });
 
@@ -70,8 +70,8 @@ app.get(
                 .json({ error: `Method ${req.method} not allowed` });
         }
         res.status(200).json({
-            brawlers: brawlers,
-            players: players,
+            brawlers: brawlStarsBrawlers,
+            players: brawlStarsPlayers,
         });
     }
 );
@@ -86,10 +86,10 @@ app.get(
                 .json({ error: `Method ${req.method} not allowed` });
         }
         res.status(200).json({
-            kwamis: kwamis,
-            holders: holders,
-            akumatized: akumatized,
-            amokatized: amokatized,
+            kwamis: miraculousKwamis,
+            holders: miraculousHolders,
+            akumatized: miraculousAkumatized,
+            amokatized: miraculousAmokatized,
         });
     }
 );
@@ -107,7 +107,7 @@ app.get(
         res.status(200).json({
             description:
                 "The Mughal Empire, Mogul or Moghul Empire, was an early-modern empire in South Asia. For some two centuries, the empire stretched from the outer fringes of the Indus basin in the west, northern ,in the northwest, and  Kashmir in the north,  the highlands of present-day Assam and Bangladesh in the east, and the uplands of the Deccan plateau in south India.  The Mughal empire is conventionally said to have been founded in 1526 by Babur, a warrior chieftain from what today is Uzbekistan, who employed aid from the neighboring Safavid and Ottoman empires, to defeat the Sultan of Delhi, Ibrahim Lodhi, in the First Battle of Panipat, and to sweep down the plains of Upper India.",
-            kings: kings,
+            kings: mughalEmpireKings,
         });
     }
 );
@@ -120,11 +120,11 @@ app.get("/api/jak", async (req, res: Response<JAK | { error: string }>) => {
             .json({ error: `Method ${req.method} not allowed` });
     }
     res.status(200).json({
-        hobby: hobby,
-        pictures: pictures,
-        games: games,
-        fav_food: fav_food,
-        social_medias: social_medias,
+        hobby: jakHobby,
+        pictures: jakPictures,
+        games: jakGames,
+        fav_food: jakFavFood,
+        social_medias: jakSocialMedias,
     });
 });
 

738ce03c0a34c409c8a59618e33b6797dda78a86
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-02-14 17:47:07 +0530

    was working on my game so all my other projects were going downhill, time to fix em all

--- src/data/jak/games.ts
+++ src/data/jak/games.ts
@@ -1,5 +1,16 @@
 import { JAK } from "../../typings";
 
-const games: JAK["games"] = [];
+const games: JAK["games"] = [
+    {
+        id: 0,
+        platform: "PC",
+        value: "JAK Ultimate Adventure Game",
+        description:
+            "In this RPG adventure game, players navigate through various environments, collecting crystals and defeating enemies in order to complete tasks and progress through the game.",
+        imageURL:
+            "https://img.itch.zone/aW1nLzExMzEzODczLnBuZw==/original/ARW%2BUt.png",
+        link: "https://jonak-adipta-kalita.itch.io/jak-ultimate-adventure-game",
+    },
+];
 
 export default games;

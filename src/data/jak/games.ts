a71df594d1d5fe4eb775f2aaf7bfe00accea96b9
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-03-30 15:27:39 +0530

    brand new game

--- src/data/jak/games.ts
+++ src/data/jak/games.ts
@@ -10,6 +10,15 @@ const games: JAK["games"] = [
         imageURL: "/images/jak/games/jak-utimate-adventure-game.png",
         link: "https://jonak-adipta-kalita.itch.io/jak-ultimate-adventure-game",
     },
+	{
+        id: 1,
+        platform: "PC",
+        value: "JAK Treasure Collectors Game",
+        description:
+            "In this game, the player is tasked with exploring a specific biome and collecting all the necessary items scattered throughout the area. The objective is to complete the task in the shortest amount of time possible, making it a thrilling race against the clock. The game presents various challenges and obstacles that the player must overcome to collect the items successfully.",
+        imageURL: "/images/jak/games/jak-treasure-collectors-game.png",
+        link: "https://jonak-adipta-kalita.itch.io/jak-treasure-collectors-game",
+    },
 ];
 
 export default games;
